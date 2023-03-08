5a66178e086ab3331ad74ef9570c64eb0c29330e
Lucas-O-S <127321390+Lucas-O-S@users.noreply.github.com>
2023-03-08 14:26:40 -0300

    Ex 4
    4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
    
    SP – R$67.836,43
    RJ – R$36.678,66
    MG – R$29.229,88
    ES – R$27.165,48
    Outros – R$19.849,53
    
    Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] local = new string[5] { "SP","RJ","MG","ES","Outros" };
+            double[] faturamento = new double[5] { 67836.43, 36678.66, 29229.88, 27165.48, 19849.53 };
+            double[] porcent = new double[5];
+            double Soma = 0;
+
+            for (int i=0; i <local.Length;i++)
+            {
+                Soma += faturamento[i];
+            }
+
+            for (int i = 0; i < local.Length; i++)
+            {
+                porcent[i] = (faturamento[i]/Soma)*100;
+                Console.WriteLine($"Local: {local[i]}, Faturmento R${faturamento[i]}, Porcentagem {porcent[i]:F2}%");
+            }
+        }
+    }
+}
