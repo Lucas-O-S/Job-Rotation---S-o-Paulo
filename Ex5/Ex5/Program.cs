6475bd8a16abed5da46dddd2942c335a778f7bdb
Lucas-O-S <127321390+Lucas-O-S@users.noreply.github.com>
2023-03-08 14:27:59 -0300

    Ex 5
    5) Escreva um programa que inverta os caracteres de um string.
    
    IMPORTANTE:
    a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
    b) Evite usar funções prontas, como, por exemplo, reverse;

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            Console.Write("Digite algo: ");
+            texto = Console.ReadLine();
+            Console.Write($"O texto invertido é: ");
+            
+            for(int i = texto.Length -1 ; i >= 0;i--)
+            {
+                Console.Write(texto[i]);
+            }
+        }
+    }
+}
