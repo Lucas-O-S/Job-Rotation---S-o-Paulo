8fb5c9f9074ad1af0f273f30038fbd6b588829de
Lucas-O-S <127321390+Lucas-O-S@users.noreply.github.com>
2023-03-08 14:24:10 -0300

    Ex 2
    2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, soma=0, i=1,temp;
+            bool pertence = false;
+            Console.Write("Digite um valor para verificar se ele pertence a sequencia de fibonacci: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while(soma <= num)
+            {
+
+                temp = soma;
+                soma = i;
+
+                i += temp;
+
+                if (i == num || num == 0)
+                {
+                    pertence=true;
+                }
+            }
+
+            if (pertence == true)
+            {
+                Console.WriteLine($"Numero {num} esta presente na sequencia de fibonacci");
+            }
+            else
+            {
+                Console.WriteLine($"Numero {num} não esta presente na sequencia de fibonacci");
+
+            }
+
+
+        }
+    }
+}
