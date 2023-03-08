faace33d87a1512eb3adc06e7262c03e2c45452e
Lucas-O-S <127321390+Lucas-O-S@users.noreply.github.com>
2023-03-08 14:25:32 -0300

    Ex3
    3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
    • O menor valor de faturamento ocorrido em um dia do mês;
    • O maior valor de faturamento ocorrido em um dia do mês;
    • Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
    
    IMPORTANTE:
    a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
    b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float maior = 0, menor = 999999, Soma=0, media;
+            int count, countAcimaMedia=0;
+          
+
+            string json = File.ReadAllText("dados.json");
+            dynamic info = JsonConvert.DeserializeObject(json);
+
+            int[] dia = new int[info.Count];
+            float[] valor = new float[info.Count];
+            count = info.Count;
+
+
+            for (int i = 0; i< info.Count; i++)
+            {
+                dia[i] = info[i].dia;
+                valor[i] = info[i].valor;
+
+
+                if(valor[i] > maior)
+                {
+                    maior = valor[i];
+
+                }
+
+                if (valor[i] < menor)
+                {
+                    menor = valor[i];
+
+                }
+
+                if (valor[i] == 0){
+                    count--;
+                }
+                Soma += valor[i];
+
+
+            }
+
+            media = Soma / count;
+
+            for(int i = 0; i < info.Count; i++)
+            {
+                if (valor[i] > media)
+                {
+                    countAcimaMedia += 1;
+                }
+            }
+
+            Console.WriteLine("dias: " + string.Join(", ", dia));
+            Console.WriteLine("dias: " + string.Join(", ", valor));
+            Console.WriteLine("media: " + string.Join(", ", media));
+
+
+            Console.WriteLine($"Maior faturamento foi: {maior}\nMenor faturamento foi: {menor}\nHouve {countAcimaMedia} dias com faturamento acima da media");
+
+
+
+
+        }
+    }
+}
